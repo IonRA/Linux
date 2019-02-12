72dbf45f700b1e38f0cd5550aed78769385b50e0
IonRA <36458040+IonRA@users.noreply.github.com>
2019-02-12 17:15:52 +0200

    Update handler.h

--- Signals/handler.h
+++ Signals/handler.h
@@ -1,6 +1,16 @@
 #ifndef HANDLER_H
 #define HANDLER_H
 
+extern int SIGTABLE[26];
+
+extern int msg_not_dlvd;
+
+extern int rcv_pid;
+
+extern int eol;
+
+void init_sig_table();
 void handle_sig_talk(int signum);
+void handle_ack(int signum);
 
 #endif
