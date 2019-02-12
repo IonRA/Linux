b6f2944caac770610d521d4fb06a6f317234044c
IonRA <36458040+IonRA@users.noreply.github.com>
2019-02-12 17:15:12 +0200

    Update handler.c
    Add handlers.

--- Signals/handler.c
+++ Signals/handler.c
@@ -1,9 +1,35 @@
+#include "handler.h"
 #include <signal.h>
 #include <unistd.h>
-#include "handler.h"
+
+int SIGTABLE[26];
+int msg_not_dlvd = 0;
+int eol = 0;
+
+__pid_t rcv_pid = 0;
+
+void init_sig_table()
+{
+    int sig = 1;
+    int c = 0;
+
+    while (c != 26)
+    {
+        if (sig != SIGKILL && sig != SIGCONT && sig != SIGSTOP)
+        {
+            SIGTABLE[c] = sig;
+            ++c;
+        }
+
+        ++sig;
+    }
+}
 
 void handle_sig_talk(int signum)
 {
+    if (signum == 31)
+        eol = 1;
+
     char sigMessage[32];
 
     char mss = 'a';
@@ -24,4 +50,12 @@ void handle_sig_talk(int signum)
     sigMessage[31] = '\n';
 
     write(STDOUT_FILENO, sigMessage + signum , 1);
+
+    kill(rcv_pid, SIGRTMIN);
+}
+
+void handle_ack(int signum)
+{
+    if (signum != SIGRTMIN)
+        msg_not_dlvd = 1;
 }
